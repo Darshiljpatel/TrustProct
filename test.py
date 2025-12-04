e35dd448809b5053649a6204038aab561dc51d0b
Priyansh Shukla <priyanshshukla715i@gmail.com>
2025-12-05 03:06:01 +0530

    Clean up unused library imports and prints
    Removed import statements and version print statements for libraries.

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-import cv2
-import sklearn
-import numpy
-import pandas as pd
 
-print("✅ OpenCV version:", cv2.__version__)
-print("✅ NumPy version:", numpy.__version__)
-print("✅ scikit-learn version:", sklearn.__version__)
-print("✅ pandas version:", pd.__version__)
